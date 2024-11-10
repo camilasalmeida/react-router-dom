3b294725451eadd3fdaea815aec6eb7292c95924
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-10 01:20:26 -0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import PokemonList from "./components/PokemonList"
+import { useState } from "react"
 
+const initialState = [
+  { _id: 1, name: "bulbasaur", weight: 69, height: 7 },
+  { _id: 2, name: "ivysaur", weight: 130, height: 10 },
+  { _id: 3, name: "venusaur", weight: 1000, height: 20 },
+  { _id: 4, name: "charmander", weight: 85, height: 6 },
+  { _id: 5, name: "charmeleon", weight: 190, height: 11 },
+]
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  const [pokemon, setPokemon] = useState(initialState)
+  console.log(`1. Pokemon Array: `, pokemon)
 
-export default App;
+  return (
+    <>
+      <h1>Pokemon!🔴🟡⚪️</h1>
+      <PokemonList pokemon={pokemon} />
+    </>
+  )
+}
 
-
-
+export default App
